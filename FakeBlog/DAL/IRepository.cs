3518b3535424d02bb88030e801ebc10c799a7041
sward42 <sarahward42@gmail.com>
2017-03-17 14:44:15 -0500

    add previous changes

---
+++ FakeBlog/DAL/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using FakeBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.DAL
+{
+    public class IRepository
+    {
+        void AddDraft(string title, string contents, ApplicationUser author);
+    }
+}
