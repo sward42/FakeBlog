04dbb08eec4abbaacb32db94376c3d951a56904d
sward42 <sarahward42@gmail.com>
2017-03-17 14:54:08 -0500

    create interface

--- FakeBlog/DAL/IRepository.cs
+++ FakeBlog/DAL/IRepository.cs
@@ -6,8 +6,21 @@ using System.Web;
 
 namespace FakeBlog.DAL
 {
-    public class IRepository
+    public interface IRepository
     {
-        void AddDraft(string title, string contents, ApplicationUser author);
+        void CreateDraft(string title, string contents, ApplicationUser author);
+        void PublishDraft(int draftId);
+        void AddPost(string title, string contents, ApplicationUser author);
+
+        bool DeletePost(int postId);
+        bool DeleteDraft(int draftId);
+
+        bool EditDraft(int draftId, string newContents);
+        bool EditPost(int postId, string newContents);
+
+        List<Draft> ReturnDrafts(string authorId);
+        Draft ReturnSingleDraft(string authorId, int draftId);
+        List<PublishedPost> ReturnPosts(string authorId);
+        PublishedPost ReturnSinglePost(string authorId, string postTitle);
     }
 }
